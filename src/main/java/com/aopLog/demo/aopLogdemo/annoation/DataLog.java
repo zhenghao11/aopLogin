7754dd6e37541dafb970ef562c3a1441a6d8c10a
z475423734 <475423734@qq.com>
2018-05-18 17:05:31 +0800

    增加注解类的注释

--- src/main/java/com/aopLog/demo/aopLogdemo/annoation/DataLog.java
+++ src/main/java/com/aopLog/demo/aopLogdemo/annoation/DataLog.java
@@ -3,11 +3,13 @@ package com.aopLog.demo.aopLogdemo.annoation;
 import java.lang.annotation.*;
 
 /**
+ * 日志注解
+ *
  * @Author hzheng
  * @Date 2017/11/8
  */
 @Documented
-@Target({ElementType.FIELD,ElementType.TYPE,ElementType.METHOD})
+@Target({ElementType.FIELD, ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DataLog {
     String name();
